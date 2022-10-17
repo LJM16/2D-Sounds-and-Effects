cb6b8883d2b51fec8a8395f7395136884362f5f9
LJM16 <111007474+LJM16@users.noreply.github.com>
2022-10-17 11:31:34 -0700

    commit to maine(the state)

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 10;
     public bool isOnGround = false;
+    public bool isGameOver = false;
 
     private Rigidbody2D _playerRB;
     private Animator _playerAnim;
@@ -20,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
         {
             _playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
@@ -34,6 +35,10 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
             _playerAnim.SetBool("IsOnGround", true);
+        }
+        else if(other.gameObject.CompareTag("Obstacle"))
+        {
+            isGameOver = true;
         }
     }
 }
